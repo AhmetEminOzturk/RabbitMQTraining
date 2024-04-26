de6540d53c610a610cccf8e57405c1acb903bb35
Ahmet Emin Öztürk <eminahmetozturk@gmail.com>
2024-04-26 09:47:33 +0300

    Initial commit

---
+++ RabbitMQTraining.publisher/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory();
+        factory.Uri = new Uri("amqps://hhrxgvyw:nLanLp4m523bGry9nMprBsATwjmblzBy@cow.rmq2.cloudamqp.com/hhrxgvyw");
+
+        using var connection = factory.CreateConnection();
+
+        var channel = connection.CreateModel();
+
+        channel.QueueDeclare("hello-queue", true, false, false);
+
+        Enumerable.Range(1, 50).ToList().ForEach(x =>
+        {
+            string message = $"Message{x}";
+            
+            var messageBody = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+
+            Console.WriteLine($"Mesajınız gönderilmiştir: {message}");
+        });
+
+
+        Console.ReadLine();
+    }
+}
---
+++ RabbitMQTraining.subscriber/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory();
+        factory.Uri = new Uri("amqps://hhrxgvyw:nLanLp4m523bGry9nMprBsATwjmblzBy@cow.rmq2.cloudamqp.com/hhrxgvyw");
+
+        using var connection = factory.CreateConnection();
+
+        var channel = connection.CreateModel();
+
+        //channel.QueueDeclare("hello-queue", true, false, false);
+
+
+        channel.BasicQos(0,1,false);
+        var consumer = new EventingBasicConsumer(channel);
+        
+        channel.BasicConsume("hello-queue", false, consumer);
+        
+        consumer.Received+= (object sender, BasicDeliverEventArgs e)=>
+        {
+            var message = Encoding.UTF8.GetString(e.Body.ToArray());
+            
+            Thread.Sleep(1500);
+            Console.WriteLine("Gelen Mesaj:" + message);
+
+            channel.BasicAck(e.DeliveryTag,false);
+        };
+        
+        Console.ReadLine();
+    }
+}
